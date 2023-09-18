ffb0c0a1c5a245f67e3b35ce831ad9e1e41beb43
crojas <candyrojas579@gmail.com>
2023-09-17 20:58:45 -0600

    Segundo enunciado -I PARTE.

---
+++ monitoreo_procesos.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+read -p "INGRESE EL NOMBRE DEL PROCESO:" PROCESO
+read -p "INGRESE EL COMANDO PARA EJECUTARLO: " COMANDO
+
+PROCESOS=$(ps -eo comm=)
+encontrado=0
+
+for proceso in $PROCESOS
+do
+	if [ $PROCESO == $proceso ]; then
+		encontrado=1
+		break
+	fi
+done
+
+if [ $encontrado -eq 0 ]; then
+	echo "EL PROCESO NO EXISTE."
+
+else
+	echo "PROCESO ENCONTRADO."
+	if [ "$(pgrep -x "$PROCESO")" > /dev/null ]; then
+		echo "EL PROCESO YA ESTÀ EN EJECUCIÒN."
+	else
+		eval $COMANDO
+		echo "SE HA INICIADO EL PROCESO $PROCESO"
+	fi
+
+fi
